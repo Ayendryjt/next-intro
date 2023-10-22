7c42c29420936edafd86146d02b639d796310af2
Ayendry <ayendryjt@gmail.com>
2023-10-22 16:45:53 -0400

    Confirm the components of the tutorial.

--- next-intro/app/page.tsx
+++ next-intro/app/page.tsx
@@ -1,116 +1,75 @@
-import Image from 'next/image'
+import { Header } from "./components/Header";
+import { SecondaryTitle } from "./components/SecondaryTitle";
+import { VoteSection } from "./components/VoteSection";
+import { Footer } from "./components/Footer";
+
+import {
+  extraSection,
+  firstSection,
+  secondSection,
+  thirdSection,
+  fourthSection,
+  fifthSection,
+  sixthSection,
+  seventhSection,
+  EighthSection,
+  ninthSection,
+  tenthSection,
+  eleventhSection, 
+} from "./sectionsData";
+
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>
-       Ayendry Jimenez 2020-0692
-      </h1>
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
+    <div className="bg-yellow-400 min-h-screen flex flex-col">
+      <Header />
+      <section className="mt-48 max-w-[1000px] mx-auto">
+        <h1 className="max-w-5xl mx-auto text-5xl font-extrabold text-center text-black md:text-8xl">
+          Vota los mejores del desarrollo web
+        </h1>
+        <p className="max-w-lg mx-auto mt-10 text-2xl text-center text-black opacity-70">
+          Decide qué stack, tecnologías y frameworks han sido los mejores del
+          año. ¡Cada voto cuenta!
         </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '} 
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+        <div className="max-w-4xl p-10 mx-auto my-20 bg-white border-2 border-black rounded-lg">
+          <SecondaryTitle title="Mejor nueva funcionalidad ECMAScript" />
+          <VoteSection section={extraSection} />
+
+          <SecondaryTitle title="Mejor biblioteca UI" />
+          <VoteSection section={firstSection} />
+
+          <SecondaryTitle title="Mejor framework Backend" />
+          <VoteSection section={secondSection} />
+
+          <SecondaryTitle title="Mejor framework FullStack" />
+          <VoteSection section={thirdSection} />
+
+          <SecondaryTitle title="Mejores empaquetadores de código" />
+          <VoteSection section={fourthSection} />
+
+          <SecondaryTitle title="Mejor entorno de ejecución de JavaScript" />
+          <VoteSection section={fifthSection} />
+
+          <SecondaryTitle title="Mejor librería CSS en JS" />
+          <VoteSection section={sixthSection} />
+
+          <SecondaryTitle title="Mejor herramienta de testing" />
+          <VoteSection section={seventhSection} />
+
+          <SecondaryTitle title="Mejor test E2E" />
+          <VoteSection section={EighthSection} />
+
+          <SecondaryTitle title="Mejor BaaS (Backend as a Service)" />
+          <VoteSection section={ninthSection} />
+
+          <SecondaryTitle title="Mejor catálogo de componentes" />
+          <VoteSection section={tenthSection} />
+
+          <SecondaryTitle title="Mejor sitio para hosting de tu proyecto web" />
+          <VoteSection section={eleventhSection} />
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+      </section>
+      <Footer />
+    </div>
+  );
 }
---
+++ next-intro/app/sectionsData.tsx
@@ -0,0 +1,284 @@
+import { ReactIcon } from "./components/icons/ReactIcon";
+import { VueIcon } from "./components/icons/VueIcon";
+import { AngularIcon } from "./components/icons/AngularIcon";
+import { SvelteIcon } from "./components/icons/SvelteIcon";
+import { LitIcon } from "./components/icons/LitIcon";
+import { NestIcon } from "./components/icons/NestIcon";
+import { ExpressIcon } from "./components/icons/ExpressIcon";
+import { FastifyIcon } from "./components/icons/FastifyIcon";
+import { HapiIcon } from "./components/icons/HapiIcon";
+import { NextIcon } from "./components/icons/NextIcon";
+import { NuxtIcon } from "./components/icons/NuxtIcon";
+import { SvelteKitIcon } from "./components/icons/SvelteKitIcon";
+import { RemixIcon } from "./components/icons/RemixIcon";
+import { AstroIcon } from "./components/icons/AstroIcon";
+import { BlitzJSIcon } from "./components/icons/BlitzJSIcon";
+import { WebpackIcon } from "./components/icons/WebpackIcon";
+import { ParcelIcon } from "./components/icons/ParcelIcon";
+import { RollupIcon } from "./components/icons/RollupIcon";
+import { EsBuildIcon } from "./components/icons/EsBuildIcon";
+import { ViteIcon } from "./components/icons/ViteIcon";
+import { NodeIcon } from "./components/icons/NodeIcon";
+import { DenoIcon } from "./components/icons/DenoIcon";
+import { StitchesIcon } from "./components/icons/StitchesIcon";
+import { StyledComponentsIcon } from "./components/icons/StyledComponentsIcon";
+import { EmotionIcon } from "./components/icons/EmotionIcon";
+import { JestIcon } from "./components/icons/JestIcon";
+import { TestingLibraryIcon } from "./components/icons/TestingLibraryIcon";
+import { VitestIcon } from "./components/icons/VitestIcon";
+import { ECMAScriptTextIcon } from "./components/icons/ECMAScriptTextIcon";
+import { CypressIcon } from "./components/icons/CypressIcon";
+import {PlaywrightIcon} from "./components/icons/PlaywrightIcon"
+import {PuppeteerIcon} from "./components/icons/PuppeteerIcon"
+import { VoteSection } from "./components/VoteSection";
+import {FirebaseIcon} from "./components/icons/FirebaseIcon"
+import {SupabaseIcon} from "./components/icons/SupabaseIcon"
+import {AirtableIcon} from "./components/icons/AirtableIcon"
+import {AmplifyIcon} from "./components/icons/AmplifyIcon"
+import {MongoIcon} from "./components/icons/MongoIcon"
+import { ChakraUiIcon } from "./components/icons/ChakraUiIcon";
+import { MaterialUiIcon } from "./components/icons/MaterialUiIcon";
+import { NextUiIcon } from "./components/icons/NextUiIcon";
+import { AntDesingIcon } from "./components/icons/AntDesingIcon";
+import { DaisyUiIcon } from "./components/icons/DaisyUiIcon";
+import { ReactSemanticIcon } from "./components/icons/ReactSemanticIcon";
+import { VerselIcon } from "./VerselIcon";
+import { NetlifyIcon } from "./components/icons/NetlifyIcon";
+import { RenderIcon } from "./components/icons/RenderIcon";
+import { AWSIcon } from "./components/icons/AWSIcon";
+import { AzureIcon } from "./components/icons/AzureIcon";
+import { GCloudIcon } from "./components/icons/GCloudIcon";
+
+
+export const firstSection = [
+  {
+    name: "React",
+    element: <ReactIcon />,
+  },
+  {
+    name: "Vue.js",
+    element: <VueIcon />,
+  },
+  {
+    name: "Angular",
+    element: <AngularIcon />,
+  },
+  {
+    name: "Svelte",
+    element: <SvelteIcon />,
+  },
+  {
+    name: "Lit",
+    element: <LitIcon />,
+  },
+];
+
+export const secondSection = [
+  {
+    name: "Nest.js",
+    element: <NestIcon />,
+  },
+  {
+    name: "Express",
+    element: <ExpressIcon />,
+  },
+  {
+    name: "Fastify",
+    element: <FastifyIcon />,
+  },
+  {
+    name: "Hapi",
+    element: <HapiIcon />,
+  }
+];
+
+export const thirdSection = [
+  {
+    name: "Next.js",
+    element: <NextIcon />,
+  },
+  {
+    name: "Nuxt",
+    element: <NuxtIcon />,
+  },
+  {
+    name: "SvelteKit",
+    element: <SvelteKitIcon />,
+  },
+  {
+    name: "Remix",
+    element: <RemixIcon />,
+  },
+  {
+    name: "Astro",
+    element: <AstroIcon />,
+  },
+  {
+    name: "BlitzJS",
+    element: <BlitzJSIcon />,
+  }
+];
+
+export const fourthSection = [
+  {
+    name: "Webpack",
+    element: <WebpackIcon />,
+  },
+  {
+    name: "Rollup",
+    element: <RollupIcon />,
+  },
+  {
+    name: "Parcel",
+    element: <ParcelIcon />,
+  },
+  {
+    name: "EsBuild",
+    element: <EsBuildIcon />,
+  },
+  {
+    name: "Vite",
+    element: <ViteIcon />,
+  }
+];
+
+export const fifthSection = [
+  {
+    name: "Node.js",
+    element: <NodeIcon />,
+  },
+  {
+    name: "Deno",
+    element: <DenoIcon />,
+  }
+];
+
+export const sixthSection = [
+  {
+    name: "Stitches",
+    element: <StitchesIcon />,
+  },
+  {
+    name: "Styled Components",
+    element: <StyledComponentsIcon />,
+  },
+  {
+    name: "Emotion",
+    element: <EmotionIcon />,
+  }
+];
+
+export const seventhSection = [
+  {
+    name: "Jest",
+    element: <JestIcon />,
+  },
+  {
+    name: "Testing Library",
+    element: <TestingLibraryIcon />,
+  },
+  {
+    name: "Vitest",
+    element: <VitestIcon />,
+  }
+];
+
+export const EighthSection = [
+   {
+    name: "Cypress",
+    element: <CypressIcon />,
+   },
+   {
+    name: "Playwright",
+    element: <PlaywrightIcon />,
+   },
+   {
+    name: "Puppeteer",
+    element: <PuppeteerIcon />,
+   }
+];
+export const ninthSection = [
+  {
+    name: "Firebase",
+    element: <FirebaseIcon />,
+  },
+  {
+    name: "Supabase",
+    element: <SupabaseIcon />,
+  },
+  {
+    name: "Airtable",
+    element: <AirtableIcon />,
+  },
+  {
+    name: "Amplify",
+    element: <AmplifyIcon />,
+  },
+  {
+    name: "Mongo",
+    element: <MongoIcon />,
+  },
+
+];
+export const tenthSection = [
+  {
+    name: "Chakra UI",
+    element: <ChakraUiIcon />
+  },
+  {
+    name: "Material UI",
+    element: <MaterialUiIcon />
+  },
+  {
+    name: "Next UI",
+    element: <NextUiIcon />
+  },
+  {
+    name: "Ant Design",
+    element: <AntDesingIcon />
+  },
+  {
+    name: "Daisy UI",
+    element: <DaisyUiIcon />
+  },
+  {
+    name: "React Semantic UI",
+    element: <ReactSemanticIcon />
+  },
+]
+
+export const eleventhSection = [
+  {
+    name: "Versel",
+    element: <VerselIcon />
+  },
+  {
+    name: "Netlify",
+    element: <NetlifyIcon />
+  },
+  {
+    name: "Render",
+    element: <RenderIcon />
+  },
+  {
+    name: "AWS",
+    element: <AWSIcon />
+  },
+  {
+    name: "Azure",
+    element: <AzureIcon />
+    
+  },
+  {
+    name: "Google Cloud",
+    element: <GCloudIcon />
+  },
+]
+
+export const extraSection = [
+  {
+    name: "Nullish Coalescing Operator",
+    element: <ECMAScriptTextIcon />,
+  }
+  
+];
