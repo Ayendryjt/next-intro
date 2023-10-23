369fe1a170f2a514d7b6cced94a6aab4af605064
Bi <116228374+bianny1107@users.noreply.github.com>
2023-10-23 12:42:08 -0400

    .

--- next-intro/app/components/Footer.tsx
+++ next-intro/app/components/Footer.tsx
@@ -4,15 +4,18 @@ export function Footer() {
     return (
       <footer className="flex flex-center justify-center w-full py-12 mt-8 border-t-2 border-t-black">
         <div className="">
+          <div className="flex justify-center">
+          </div>
+          <span>&bull;</span>
           <a
             className="px-1 border-dashed border-b-2 border-b-yellow-js hover:border-black"
-            href="https://github.com/Ayendryjt/next-intro.git"
+            href="https://github.com/Chd3451/next-intro.git"
             rel="nofollow noreferrer"
             target="_blank"
           >
             GitHub
           </a>
-          <span>&bull;</span>
+          <span>&bull;</span> 
         </div>
       </footer>
     )
